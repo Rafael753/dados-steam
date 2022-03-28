b16088497a4b406fe8b71468ce8c35aff5f7401d
Amanda <amanda.alexandre@outlook.com>
2022-03-28 10:54:30 -0300

    Excluindo indie dos gêneros

--- queries/generos_competicao.sql
+++ queries/generos_competicao.sql
@@ -6,3 +6,9 @@ SELECT genres, SUM(appid) AS jogos_por_genero FROM jogos GROUP BY genres ORDER B
 
 -- quais gêneros têm menos competição?
 SELECT genres, SUM(appid) AS jogos_por_genero FROM jogos GROUP BY genres ORDER BY jogos_por_genero LIMIT 10;
+
+-- quais gêneros têm mais competição (EXCLUNDO INDIES)?
+SELECT genres, SUM(appid) AS jogos_por_genero FROM jogos WHERE genres NOT LIKE '%Indie%' GROUP BY genres ORDER BY jogos_por_genero DESC LIMIT 10;
+
+-- quais gêneros têm menos competição (EXCLUNDO INDIES)?
+SELECT genres, SUM(appid) AS jogos_por_genero FROM jogos WHERE genres NOT LIKE '%Indie%' GROUP BY genres ORDER BY jogos_por_genero LIMIT 10;
