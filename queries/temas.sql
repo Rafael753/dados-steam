cef2f6c016b38825f8575b80ba0d0521fa905f15
Amanda <amanda.alexandre@outlook.com>
2022-03-28 17:33:45 -0300

    Priorização de perguntas

---
+++ queries/temas.sql
@@ -0,0 +1,15 @@
+USE steam_dados;
+
+-- mostra descrições;
+SELECT * FROM steam_description_data;
+
+-- une os jogos com suas descrições
+SELECT jogos.appid, jogos.name, jogos.genres, steam_description_data.detailed_description FROM jogos
+JOIN steam_description_data
+ON jogos.appid = steam_description_data.steam_appid;
+
+-- seleciona jogos com o tema: medieval;
+SELECT jogos.appid, jogos.name, jogos.genres, steam_description_data.detailed_description FROM jogos
+JOIN steam_description_data
+ON jogos.appid = steam_description_data.steam_appid
+WHERE steam_description_data.detailed_description LIKE '%magic%'; 
